47807578eca73a521f9725b384f797e2f8acddb5
Pat Haynes <pathaynes32@gmail.com>
2019-08-08 16:28:12 -0700

    new stories

--- javascript/results.js
+++ javascript/results.js
@@ -15,17 +15,17 @@ if(level <= 2) {
             storyLine.textContent = 'Mr T. might pity the fool but you\'ve proved yourself not to be a fool! Now that you have all of Mr T\'s money it\'s time to test your wits because this next boss will challenge your intellect while stealing your money at the same time.';
             continueButton.textContent = 'Continue';
         } else {
-            storyLine.textContent = 'Go hone your kung fu.';
+            storyLine.textContent = 'I\'ve seen a lot of fools in my day and you might be the biggest fool of them all. I PITY THE FOOL! Go back to school and learn how to not be a fool... ya damn fool.';
             continueButton.textContent = 'Restart';
             continueButton.setAttribute('onclick', "window.location.href = '../index.html'");
             aboutUsButton.classList.remove('hidden');
         }
     } else if(level === 1) {
         if(playerMoney > 0) {
-            storyLine.textContent = 'Inconceivable!';
+            storyLine.textContent = 'Inconceivable! Looks like Vizzini is drinking bad merlot, he will have to settle for some box wine from the local grocery outlet tonight because we just took all his money. Good thing too because your next opponent is a real shark.... actually we are pretty sure he is a giant worm but either way he smells, he cheats, and he has a lot of money to bet..... Good luck.';
             continueButton.textContent = 'Continue';
         } else {
-            storyLine.textContent = 'The only thing worse than a bad merlot is a moron. Looks like Vizzini is drinking bad merlot, he will have to settle for some box wine from the local grocery outlet tonight because we just took all his money. Good thing too because your next opponent is a real shark.... actually we are pretty sure he is a giant worm but either way he smells, he cheats, and he has a lot of money to bet..... Good luck.' ;
+            storyLine.textContent = 'The only thing worse than a bad merlot is a moron. And you good sir are among the biggest morons I have ever dueled with! They have these things called books, maybe you go home and ask your mom about them, learn how to read one, then throw yourself out a window you MORON!' ;
             continueButton.textContent = 'Restart';
             continueButton.setAttribute('onclick', "window.location.href = '../index.html'");
             aboutUsButton.classList.remove('hidden');
